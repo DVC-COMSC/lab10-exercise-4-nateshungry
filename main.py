4cf97c764f68471c3707ff006a1a6e841af203b0
nateshungry <126623504+nateshungry@users.noreply.github.com>
2023-03-13 05:41:00 +0000

    Actually completed this time

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ def main():
   celcius = int(input('Enter the Celcius Degree: ')) 
   fahrenheit = (9.0 / 5.0) * celcius + 32
   print(f' The Fahrenheit is {fahrenheit:.2f}')
-    pass
+  pass
 
 
 if __name__ == '__main__':
